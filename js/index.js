8ded0838e5d014f9dd606e09332f7dbb7f17176b
wuyyyying <yiw273@ucsd.edu>
2018-11-07 17:57:16 -0800

    js for profile

--- js/index.js
+++ js/index.js
@@ -5,3 +5,8 @@ $(function(){
 $('.avatar-minus').click(function(){
    $('.del-family').text($(this).data('id'));
 });
+
+$('.img-click').click(function(){
+   $('#hide-image').attr('src', $(this).attr('src'));
+   $('#hide-image').attr('style', "display:true");
+});
