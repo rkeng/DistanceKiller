c4332e8dccfe7eb009a1d4fa8cb46c40f41753b1
rkeng <rkeng@ucsd.edu>
2018-11-08 19:29:06 -0800

    fake authentication

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
-$(function(){
+$(function () {
    $('[data-toggle="tooltip"]').tooltip();
-});
 
-$('.avatar-minus').click(function(){
-   $('.del-family').text($(this).data('id'));
-});
-
-$('.img-click').click(function(){
-   $('#hide-image').attr('src', $(this).attr('src'));
-   $('#hide-image').attr('style', "display:true");
+   // get loginStatus and set welcome text
+   let loginStatus = JSON.parse(sessionStorage.getItem('loginStatus'));
+   $('#welcome').text(function (i, oldText) {
+      return oldText = oldText + ", " + loginStatus.user.cred.name;
+   });
 });
